858438a72fcbee87840408870361651c9a3f9c96
watlix <93144173+watlix@users.noreply.github.com>
2021-10-27 13:37:22 -0400

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Silly)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Sad)
+})
